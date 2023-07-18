97f9bdc5b3992c53706023bce75335d8716e744a
Lucas Givens <givlucas@gmail.com>
2023-07-18 18:32:50 -0400

    one button testing

--- controller.ino
+++ controller.ino
@@ -1,12 +1,19 @@
 #include "Keyboard.h"
 
+int in = 9;
+int val = 0;
+
 void setup() {
-  Keyboard.begin();
+  // Keyboard.begin();
+  pinMode(in, INPUT);
 }
 
 void loop() {
-  while(true){
-    delay(30000);
-    Keyboard.write("u");
-  }
+  // while(true){
+  //   delay(30000);
+  //   Keyboard.write("u");
+  // }
+  val = digitalRead(in);
+  Serial.print(val);
+  delay(1000);
 }
